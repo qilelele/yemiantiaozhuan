a6372eaac00db0fa10c6a120dc29ab2c8bfc3305
qilelele <2675883152@qq.com>
2021-05-10 19:40:59 +0800

    first commit

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+﻿package cn.itcast.openbrowser;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.main_button);               button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                //设置动作为android.intent.action.VIEW
+                intent.setAction("android.intent.action.VIEW");
+                //设置要打开的网址
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
